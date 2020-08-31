ede43bbd3c1e4899b351f446c6259bd76c2faa04
Mark Gun <mgunsd@gmail.com>
2020-08-31 22:20:14 +0100

    Updates

---
+++ precache-manifest.9aa640fc4135933ee5f5fce21f704d2d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea7ebd54ca2d2508a9a975beb8b00746",
+    "url": "/cra-template-emel/index.html"
+  },
+  {
+    "revision": "0c13ac8c78526eb1e8a5",
+    "url": "/cra-template-emel/static/css/main.7e40b007.chunk.css"
+  },
+  {
+    "revision": "138d0a77f8ab20d0db88",
+    "url": "/cra-template-emel/static/js/2.fd92d499.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/cra-template-emel/static/js/2.fd92d499.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0c13ac8c78526eb1e8a5",
+    "url": "/cra-template-emel/static/js/main.17ccf937.chunk.js"
+  },
+  {
+    "revision": "d42735edb9c20e64f189",
+    "url": "/cra-template-emel/static/js/runtime-main.ef49cf17.js"
+  },
+  {
+    "revision": "ff1c1ab9248c2f0037504994ab451bba",
+    "url": "/cra-template-emel/static/media/reactlogo.ff1c1ab9.svg"
+  }
+]);
